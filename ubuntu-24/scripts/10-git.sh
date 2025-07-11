48e7d946288377f45f11a0cbf4d66c697ff1fc70
default <admin@unlimitech.cloud>
2025-07-11 15:04:37 +0000

    added git global config

--- ubuntu-24/scripts/10-git.sh
+++ ubuntu-24/scripts/10-git.sh
@@ -18,6 +18,8 @@ git config --global user.email "$OWNER_EMAIL"
 echo "🧑‍ Configurando git user.name como: $OWNER_NAME"
 git config --global user.name "$OWNER_NAME"
 
+git config --global pull.rebase false
+
 echo "✅ Configuración de Git actual:"
 git config --global --get user.email | xargs -I {} echo "   - Email: {}"
 git config --global --get user.name | xargs -I {} echo "   - Nombre: {}"
